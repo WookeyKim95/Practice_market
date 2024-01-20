f9540a8f66084d89bd93a5255d2aa3572f8475b6
WookeyKim95 <bluebear9508@naver.com>
2024-01-21 08:43:46 +0900

    making login authencation

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+const NavItem = ({mobile} : {mobile?: boolean}) => {
+  const {data : session, status} = useSession()
+  return (
+    <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}`}>
+        <li className='py-2 text-center border-b-4 cursor-pointer'><Link href="/admin">Admin</Link></li>
+        <li className='py-2 text-center border-b-4 cursor-pointer'><Link href="/user">User</Link></li>
+        {session?.user
+          ? 
+          <li className='py-2 text-center border-b-4 cursor-pointer'><button onClick={() => signOut()}>Signout</button></li>
+          :
+          <li className='py-2 text-center border-b-4 cursor-pointer'><button onClick={() => signIn()}>Signin</button></li>
+        }
+        
+        
+    </ul>
+  )
+}
+
+export default NavItem
